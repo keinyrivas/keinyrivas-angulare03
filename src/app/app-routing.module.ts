7784b261f966e7b0355e23865c3d53f3ac8b7aac
keiny rivas <keinyrivas@gmail.com>
2020-08-25 19:01:35 -0300

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+    
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GifsComponent } from './gifs/gifs.component';
+import { BuscarComponent } from './buscar/buscar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: '', component: BuscarComponent },
+	{ path: 'resultados/:q', component: GifsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+    
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GifsComponent } from './gifs/gifs.component';
+import { BuscarComponent } from './buscar/buscar.component';
+import { AppRoutingModule } from './app-routing.module'
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GifsComponent,
+    BuscarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
